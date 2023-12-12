1807e971cd7ae5ea497d07ba4e2eb07c9aad8c62
devkgm <gm.kim333@gmail.com>
2023-12-12 13:46:25 +0900

    delete

--- script.js
+++ script.js
@@ -8,41 +8,45 @@ const modalContainer = document.getElementById("modalContainer");
 function modalOpen(e) {
     const modalContainer = document.getElementById("modalContainer");
     const index = e.getAttribute("index");
-    const modalContent = document.getElementById("modal_item"+index);
-    const img = document.querySelectorAll("#modal_item"+index+" .imgList li");
-    const arrowRight = document.querySelector("#modal_item"+index+" .right");
-    const arrowLeft = document.querySelector("#modal_item"+index+" .left");
+    const modalContent = document.getElementById("modal_item" + index);
+    const img = document.querySelectorAll(
+        "#modal_item" + index + " .imgList li"
+    );
+    const arrowRight = document.querySelector(
+        "#modal_item" + index + " .right"
+    );
+    const arrowLeft = document.querySelector("#modal_item" + index + " .left");
     //이미지 위치에 따라 화살표 비활성화 및 활성화
-    arrowRight.addEventListener('click',()=>{
+    arrowRight.addEventListener("click", () => {
         let imgIndex = getImageIndex(img);
-        console.log(imgIndex)
-        if(imgIndex == img.length-1){
+        console.log(imgIndex);
+        if (imgIndex == img.length - 1) {
             arrowRight.classList.add("disable");
         } else {
             arrowRight.classList.remove("disable");
         }
-        if(imgIndex != 0){
+        if (imgIndex != 0) {
             arrowLeft.classList.remove("disable");
         }
-    })
-    arrowLeft.addEventListener('click',()=>{
-        let imgIndex = getImageIndex(img)-1;
-        console.log(imgIndex)
+    });
+    arrowLeft.addEventListener("click", () => {
+        let imgIndex = getImageIndex(img) - 1;
+        console.log(imgIndex);
 
-        if(imgIndex == 0){
+        if (imgIndex == 0) {
             arrowLeft.classList.add("disable");
         } else {
             arrowLeft.classList.remove("disable");
         }
-        if(imgIndex < img.length-1){
+        if (imgIndex < img.length - 1) {
             arrowRight.classList.remove("disable");
         }
-    })
+    });
     let imgIndex = getImageIndex(img);
-    if(imgIndex == 0){
+    if (imgIndex == 0) {
         arrowLeft.classList.add("disable");
     }
-    if(img.length <= 1){
+    if (img.length <= 1) {
         arrowRight.classList.add("disable");
     }
     //모달 open
@@ -54,34 +58,33 @@ function modalOpen(e) {
 window.addEventListener("click", (event) => {
     if (event.target === modalContainer) {
         modalContainer.style.display = "none";
-        const modalContent = document.getElementById("modal_item"+selectedItem);
+        const modalContent = document.getElementById(
+            "modal_item" + selectedItem
+        );
         modalContent.style.display = "none";
     }
 });
-//DOM 로드 완료시 컨텐츠 불러오기
-document.addEventListener("DOMContentLoaded", () => {
-    //로컬 컨텐츠 불러와서 바로 컨텐츠 로드
-    fetch('./Contents.json')
-        .then(res => res.json())
-        .then(data => allContent = data)
-        .then(()=>{
-            contentsLoad(allContent);
-            classificationContent(allContent);
-        });
-})
-function classificationContent(allContent){
-    allContent.forEach((content, index)=>{
-        if(content.country == "domestic"){
+
+fetch("./Contents.json")
+    .then((res) => res.json())
+    .then((data) => (allContent = data))
+    .then(() => {
+        contentsLoad(allContent);
+        classificationContent(allContent);
+    });
+function classificationContent(allContent) {
+    allContent.forEach((content, index) => {
+        if (content.country == "domestic") {
             localContent.push(content);
         } else {
             globalContent.push(content);
         }
-    })
+    });
 }
 //컨텐츠 로드 함수
-function contentsLoad(contents){
+function contentsLoad(contents) {
     const itemContainer = document.getElementById("itemContainer");
-    const modalContainer = document.getElementById("modalContainer")
+    const modalContainer = document.getElementById("modalContainer");
     itemContainer.innerHTML = "";
     modalContainer.innerHTML = "";
     contents.map((content, index) => {
@@ -98,19 +101,19 @@ function contentsLoad(contents){
         </div>
         `;
         const imageHTMLarr = content.images.map((image, index) => {
-            if(index == 0){
+            if (index == 0) {
                 return `<li class="display"><img src="${image}" alt="image"></li>`;
             }
             return `<li><img src="${image}" alt="image"></li>`;
-        })
+        });
         const bulletArr = content.images.map((image, index) => {
-            if(index == 0){
+            if (index == 0) {
                 return `<li class="bullet">.</li>`;
             }
             return `<li>.</li>`;
-        })
-        const imageHTML = imageHTMLarr.join('');
-        const bullet = bulletArr.join('');
+        });
+        const imageHTML = imageHTMLarr.join("");
+        const bullet = bulletArr.join("");
         modalContainer.innerHTML += `
         <div class="modalContent" id="modal_item${index}">
             
@@ -148,17 +151,17 @@ function contentsLoad(contents){
             </div>
         </div>
         `;
-    })
+    });
 }
 //국내 해외 버튼 핸들러
-function handleLocationChange(e){
+function handleLocationChange(e) {
     const buttons = document.querySelectorAll("nav button");
-    if(e.id == "allBtn"){
+    if (e.id == "allBtn") {
         buttons[0].classList.add("active");
         buttons[1].classList.remove("active");
         buttons[2].classList.remove("active");
         contentsLoad(allContent);
-    } else if(e.id == "localBtn") {
+    } else if (e.id == "localBtn") {
         buttons[0].classList.remove("active");
         buttons[1].classList.add("active");
         buttons[2].classList.remove("active");
@@ -171,94 +174,92 @@ function handleLocationChange(e){
     }
 }
 //검색
-function handleSearch(){
+function handleSearch() {
     const inputLocation = document.getElementById("inputLocation").value.trim();
-    const location = document.getElementsByClassName('location');
-    const itemCard = document.getElementsByClassName('itemCard');
-    if(inputLocation == ""){
-        for(let i = 0; i< itemCard.length; i++){
-            itemCard[i].classList.remove('disable');
+    const location = document.getElementsByClassName("location");
+    const itemCard = document.getElementsByClassName("itemCard");
+    if (inputLocation == "") {
+        for (let i = 0; i < itemCard.length; i++) {
+            itemCard[i].classList.remove("disable");
         }
     } else {
         let removeCard = [];
-        for(let i = 0; i < location.length; i++){
-            if(location[i].innerText.search(inputLocation)==-1){
+        for (let i = 0; i < location.length; i++) {
+            if (location[i].innerText.search(inputLocation) == -1) {
                 removeCard.push(itemCard[i]);
             } else {
-                itemCard[i].classList.remove('disable');
+                itemCard[i].classList.remove("disable");
             }
         }
-        for(let i = 0; i< removeCard.length; i++){
-            removeCard[i].classList.add('disable');
+        for (let i = 0; i < removeCard.length; i++) {
+            removeCard[i].classList.add("disable");
         }
     }
-    
 }
 //모달 내부 이미지 핸들러
 function handleBullet(e, dir) {
     const element = e.target;
     const index = element.getAttribute("index");
-    const images = document.querySelectorAll("#modal_item" + index + " .imgList li");
-    const bullet = document.querySelectorAll("#modal_item" + index + " .bulletList li");
-    if(dir == "right"){
+    const images = document.querySelectorAll(
+        "#modal_item" + index + " .imgList li"
+    );
+    const bullet = document.querySelectorAll(
+        "#modal_item" + index + " .bulletList li"
+    );
+    if (dir == "right") {
         handleRight(images);
     } else {
         handleLeft(images);
     }
     let imgIndex = getImageIndex(images);
-    if(dir == "left") imgIndex--;
-    for(let i = 0; i < bullet.length; i++){
-        if(i == imgIndex){
-            bullet[i].classList.add('bullet');
+    if (dir == "left") imgIndex--;
+    for (let i = 0; i < bullet.length; i++) {
+        if (i == imgIndex) {
+            bullet[i].classList.add("bullet");
         } else {
-            bullet[i].classList.remove('bullet');
+            bullet[i].classList.remove("bullet");
         }
     }
-    
 }
 //슬라이드 로직
 function getImageIndex(images) {
     let index;
-    images.forEach((element,idx) => {
-        if(element.classList.contains("display")) {
-            return index = idx;
+    images.forEach((element, idx) => {
+        if (element.classList.contains("display")) {
+            return (index = idx);
         }
     });
     return index;
 }
 function handleLeft(images) {
     let index = getImageIndex(images);
-    if(index == 0) {
+    if (index == 0) {
         return;
     }
     index--;
     images[index].classList.add("display");
-    images[index+1].classList.add("slideright2");
+    images[index + 1].classList.add("slideright2");
     images[index].classList.add("slideleft2");
 
-    setTimeout(()=>{
-        images[index+1].classList.remove("slideright2");
+    setTimeout(() => {
+        images[index + 1].classList.remove("slideright2");
         images[index].classList.remove("slideleft2");
-        images[index+1].classList.remove("display");
-
-    },400)
+        images[index + 1].classList.remove("display");
+    }, 400);
 }
 function handleRight(images) {
     let index = getImageIndex(images);
-    if(index == images.length-1) {
+    if (index == images.length - 1) {
         return;
     }
     index++;
     images[index].classList.add("display");
-    images[index-1].classList.add("slideleft1");
+    images[index - 1].classList.add("slideleft1");
     images[index].classList.add("slideright1");
 
     setTimeout(() => {
-        
-        images[index-1].classList.remove("slideleft1");
-        images[index].classList.remove("slideright1"); 
-        images[index-1].classList.remove("display");
+        images[index - 1].classList.remove("slideleft1");
+        images[index].classList.remove("slideright1");
+        images[index - 1].classList.remove("display");
     }, 400);
 }
-
-    
